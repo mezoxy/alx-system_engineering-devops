1bb7c9ff72fde7563585450df1e0d1731c0a17e2
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-12-22 13:07:50 -0800

    Task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# This puppet will create a file in the dir /tmp with some content
+
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
