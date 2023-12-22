a3ac505693221ffcf76219e883d91909c507dc25
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-12-22 14:17:55 -0800

    Task 2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,18 @@
 # This puppet will instal flask from pip3
 
-exec { 'insall flask':
-  command => '/usr/bin/pip3 install flask',
-  path    => '/usr/bin',
-  unless  => '/usr/bin/pip3 show flask',
-  require => Package['python3-pip']
+$python_version = '3.8.10'
+$pip_package = 'python3-pip'
+
+package { "python${python_version}":
+  ensure => $python_version,
+}
+
+package { $pip_package:
+  ensure => present,
+}
+
+package { ['flask==2.1.0', 'werkzeug==2.1.1']:
+  ensure   => installed,
+  provider => 'pip',
+  require  => Package[$pip_package],
 }
