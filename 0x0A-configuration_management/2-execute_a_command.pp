c91ec32f9c9cfe5638953a88e0d85e24211fcca5
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-12-22 14:14:15 -0800

    Task 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,6 @@
 # Using Puppet, create a manifest that kills a process named killmenow
 
 exec { 'to kill':
-  command => 'pkill -f killmenow'
+  command  => 'pkill -f killmenow'
+  provider => 'shell'
 }
