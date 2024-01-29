e901abe857521892dd3556e570a4968586308dda
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-01-29 14:27:04 -0800

    Testing 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,21 +7,26 @@ import sys
 from json import loads
 
 
-ur = "https://jsonplaceholder.typicode.com/todos/"
-r = requests.get(ur)
-j = 0
-st = ""
-name = ""
-i = 0
+if __name__ == "__main__":
+    """
+        A  Python script that, using this REST API, for a given
+        employee ID, returns information about his/her TODO list progress.
+    """
+    ur = "https://jsonplaceholder.typicode.com/todos/"
+    r = requests.get(ur)
+    j = 0
+    st = ""
+    name = ""
+    i = 0
 
-for user in r.json():
-    if user["userId"] == int(sys.argv[1]):
-        name = user.get("name")
-        if user["completed"]:
-            j += 1
-            st += user["title"] + "$"
-        i += 1
+    for user in r.json():
+        if user["userId"] == int(sys.argv[1]):
+            name = user.get("name")
+            if user["completed"]:
+                j += 1
+                st += user["title"] + "$"
+            i += 1
 
-print("Employee {} is done with tasks({}/{}):".format(name, j, i))
-for task in st.split("$")[:-1]:
-    print("\t{}".format(task))
+    print("Employee {} is done with tasks({}/{}):".format(name, j, i))
+    for task in st.split("$")[:-1]:
+        print("\t{}".format(task))
