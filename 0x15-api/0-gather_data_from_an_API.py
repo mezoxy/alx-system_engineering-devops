a2a60c301931b30031ad4f3c91b9fe7375b8dbe7
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-01-29 14:51:53 -0800

    Testing 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -13,15 +13,16 @@ if __name__ == "__main__":
         employee ID, returns information about his/her TODO list progress.
     """
     ur = "https://jsonplaceholder.typicode.com/todos/"
+    ur1 = "https://jsonplaceholder.typicode.com/users/" + sys.argv[1]
     r = requests.get(ur)
+    r2 = requests.get(ur1)
     j = 0
     st = ""
-    name = ""
+    name = r2.json().get("name")
     i = 0
 
     for user in r.json():
         if user["userId"] == int(sys.argv[1]):
-            name = user.get("name")
             if user["completed"]:
                 j += 1
                 st += user["title"] + "$"
@@ -29,4 +30,4 @@ if __name__ == "__main__":
 
     print("Employee {} is done with tasks({}/{}):".format(name, j, i))
     for task in st.split("$")[:-1]:
-        print("\t {}".format(task))
+        print("     {}".format(task))
