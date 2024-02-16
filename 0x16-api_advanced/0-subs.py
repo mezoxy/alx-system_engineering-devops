ce2c139c396471dec36671538d1a9b012756fcb7
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-02-16 11:18:11 -0800

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,7 +12,7 @@ def number_of_subscribers(subreddit):
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         data = response.json()
-        subscribers = data['data']['subscribers']
+        subscribers = data.get('data').get('subscribers')
         return subscribers
     else:
         return 0
