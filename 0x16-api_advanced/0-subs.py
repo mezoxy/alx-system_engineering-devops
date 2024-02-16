f255502ee6969884ac14f6dbb8d71c3d076c2c8f
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-02-16 11:14:45 -0800

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,10 +6,10 @@ def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers
     to the subreddit"""
     import requests
+
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'My-User-Agent'}
     response = requests.get(url, headers=headers)
-    
     if response.status_code == 200:
         data = response.json()
         subscribers = data['data']['subscribers']
