4ab5efd4ab0e2853666bf0b19c92c00c39759343
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-02-22 17:55:26 -0800

    Task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,5 @@
+# Fix the num of failed request
+exec {
+  command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
+  path    => ['/bin', '/usr/bin', '/usr/bin']
+}
